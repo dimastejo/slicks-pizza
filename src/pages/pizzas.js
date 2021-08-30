9bba1ad8cc2a8859b80a5cf721e3f445a7bfb0e0
dimastejo <dimastejob@gmail.com>
2021-08-30 20:49:23 +0700

    init

---
+++ src/pages/pizzas.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function PizzaPage() {
+  return (
+    <div>
+      <p>Hey Im Pizza Page</p>
+    </div>
+  )
+}
---
+++ src/pages/slicemasters.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function PizzaPage() {
+  return (
+    <div>
+      <p>Hey Im Slice Masters Page</p>
+    </div>
+  )
+}
